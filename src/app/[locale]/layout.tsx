33524c1bfba37c3951ba57bce91b32f987dfa680
root <root@vmi232788.contaboserver.net>
2026-04-10 13:18:52 +0200

    feat: add Google Tag Manager (GTM-KVWGJ5H3)

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -52,12 +52,29 @@ export default async function LocaleLayout({
           <link key={loc} rel="alternate" hrefLang={loc} href={`https://say-med.com/${loc}`} />
         ))}
         <link rel="alternate" hrefLang="x-default" href="https://say-med.com/en" />
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KVWGJ5H3');` }}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
       </head>
       <body className="antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KVWGJ5H3"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <NextIntlClientProvider locale={locale} messages={messages}>
           <Header />
           <main>{children}</main>
